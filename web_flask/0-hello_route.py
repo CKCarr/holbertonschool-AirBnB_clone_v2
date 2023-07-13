dbb1a6c9ea185e8e421b7bcd7438a8eb7f8d6424
CKCarr <119520583+CKCarr@users.noreply.github.com>
2023-07-13 12:22:38 -0500

    task 0  Hello Flask! script that starts a Flask web application

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python3
-from flask import Flask
 """This is a file to start the web application"""
+from flask import Flask
+
 
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
 def hello():
+    """This function return a string when a request is made to root"""
     return 'Hello HBNB!'
 
-if __name__== '__main__':
+
+if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
