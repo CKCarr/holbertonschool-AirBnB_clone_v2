077b16db41f4311a43b60e081da5ca07f2f5e2cc
CKCarr <119520583+CKCarr@users.noreply.github.com>
2023-07-15 18:26:09 -0500

    test

--- models/state.py
+++ models/state.py
@@ -4,24 +4,21 @@ import os
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 from models.base_model import BaseModel, Base
+from models import storage
+from models.city import City
 
 
 class State(BaseModel, Base):
     """ State class """
     __tablename__ = "states"
     name = Column(String(128), nullable=False)
-
     if os.getenv('HBNB_TYPE_STORAGE') == 'db':
-        cities = relationship("City",
-                              backref="state",
-                              cascade="all, delete, delete-orphan",
-                              foreign_keys="City.state_id")
+        cities = relationship("City", backref="state",
+                              cascade="all, delete, delete-orphan")
     else:
         @property
         def cities(self):
             """city getter"""
-            from models import storage
-            from models.city import City
             city_list = []
             all_cities = storage.all(City)
             for obj in all_cities.values():
