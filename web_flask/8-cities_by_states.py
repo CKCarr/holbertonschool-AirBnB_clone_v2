919ee1a99ba692421f8706466497f3e9751e9382
CKCarr <119520583+CKCarr@users.noreply.github.com>
2023-07-18 13:33:25 -0500

    says no doc

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Starts a Flask web application."""
+""" Starts a Flask web application to render an html template """
 from flask import Flask, render_template
 from models import storage
 from models.state import State
