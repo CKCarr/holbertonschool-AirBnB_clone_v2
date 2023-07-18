cbd1efd43232d78a44eb3ead849dbcacc8b62d47
CKCarr <119520583+CKCarr@users.noreply.github.com>
2023-07-18 17:18:28 -0500

    module doc

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """ This modules writes a script that starts a Flask web application """
+
 from flask import Flask, render_template
 from models import storage
 from models.state import State
